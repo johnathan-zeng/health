4fff2034a1a6fba400206cf52cf61a55e105cf0e
johnathan-zeng <zeng.johnathan@gmail.com>
2015-05-05 19:55:34 -0400

    small adj

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-  	@posts = Post.all
+  	@posts = Post.order(user_id: :desc).all
   	@posts2 = Post.where(latitude: '39.95').all
   	@posts3 = Post.where(latitude: '40.71').all
   end
